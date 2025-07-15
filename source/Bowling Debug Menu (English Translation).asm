47072ea4054d42fa52f8df7c2f26419d5efd2d8b
Boshi <157933924+b0sh1Mods@users.noreply.github.com>
2025-07-15 01:21:39 -0400

    Add files via upload

---
+++ source/Bowling Debug Menu (English Translation).asm
@@ -0,0 +1,338 @@
+#To be inserted at 80314094
+loc_0x0:
+  li r4, 0xFFFF
+  lis r20, 0x8039
+  ori r20, r20, 0x21D4
+  lis r19, 0x504C
+  ori r19, r19, 0x4159
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2200
+  lis r19, 0x4445
+  ori r19, r19, 0x4D4F
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2204
+  lis r19, 0x2020
+  ori r19, r19, 0x4341
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2208
+  lis r19, 0x4D45
+  ori r19, r19, 0x5241
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x220C
+  lis r19, 0x2020
+  ori r19, r19, 0x4544
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2210
+  lis r19, 0x4954
+  ori r19, r19, 0x4F52
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2215
+  lis r19, 0x5354
+  ori r19, r19, 0x4147
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2219
+  lis r19, 0x4553
+  ori r19, r19, 0x2020
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x221E
+  lis r19, 0x4D4F
+  ori r19, r19, 0x4445
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2222
+  lis r19, 0x2020
+  ori r19, r19, 0x2020
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2224
+  li r19, 0x0
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2225
+  lis r19, 0x504C
+  ori r19, r19, 0x4159
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2229
+  lis r19, 0x4552
+  ori r19, r19, 0x5320
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x222C
+  li r19, 0x0
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2232
+  lis r19, 0x5245
+  ori r19, r19, 0x504C
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2236
+  lis r19, 0x4159
+  ori r19, r19, 0x204C
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x223A
+  lis r19, 0x4F43
+  ori r19, r19, 0x4B00
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2241
+  lis r19, 0x464F
+  ori r19, r19, 0x5243
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2245
+  lis r19, 0x4520
+  ori r19, r19, 0x5354
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2249
+  lis r19, 0x5249
+  ori r19, r19, 0x4B45
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x224D
+  li r19, 0x0
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2254
+  lis r19, 0x4C41
+  ori r19, r19, 0x4E45
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2258
+  li r19, 0x0
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2267
+  lis r19, 0x504C
+  ori r19, r19, 0x4143
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x226B
+  lis r19, 0x4520
+  ori r19, r19, 0x484F
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x226F
+  lis r19, 0x4C44
+  ori r19, r19, 0x4552
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2273
+  li r19, 0x0
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2282
+  lis r19, 0x4841
+  ori r19, r19, 0x4E44
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2286
+  lis r19, 0x2031
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x228D
+  lis r19, 0x4841
+  ori r19, r19, 0x4E44
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2291
+  lis r19, 0x2032
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2298
+  lis r19, 0x4841
+  ori r19, r19, 0x4E44
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x229C
+  lis r19, 0x2033
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22A3
+  lis r19, 0x4841
+  ori r19, r19, 0x4E44
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22A7
+  lis r19, 0x2034
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22AE
+  lis r19, 0x4142
+  ori r19, r19, 0x534F
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22B2
+  lis r19, 0x4C55
+  ori r19, r19, 0x5445
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22B6
+  lis r19, 0x2053
+  ori r19, r19, 0x504C
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22BA
+  lis r19, 0x4954
+  ori r19, r19, 0x2D
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22C9
+  lis r19, 0x3E00
+  ori r19, r19, 0x5200
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22CD
+  lis r19, 0x4C
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22D1
+  lis r19, 0x4445
+  ori r19, r19, 0x4641
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22D5
+  lis r19, 0x554C
+  ori r19, r19, 0x5400
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22D8
+  li r19, 0x0
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22E0
+  li r19, 0x0
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22E3
+  lis r19, 0x4445
+  ori r19, r19, 0x4641
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x22E7
+  lis r19, 0x554C
+  ori r19, r19, 0x5400
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2310
+  li r19, 0x0
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x236C
+  lis r19, 0x5452
+  ori r19, r19, 0x4149
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2370
+  lis r19, 0x4E49
+  ori r19, r19, 0x4E47
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2374
+  li r19, 0x0
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2379
+  lis r19, 0x4649
+  ori r19, r19, 0x544E
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x237D
+  lis r19, 0x4553
+  ori r19, r19, 0x5300
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2358
+  lis r19, 0x3100
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x235D
+  lis r19, 0x3200
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2362
+  lis r19, 0x3300
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2367
+  lis r19, 0x3400
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2338
+  lis r19, 0x4E4F
+  ori r19, r19, 0x4E45
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2333
+  lis r19, 0x5452
+  ori r19, r19, 0x5545
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x233D
+  lis r19, 0x454E
+  ori r19, r19, 0x4142
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x2341
+  lis r19, 0x4C45
+  ori r19, r19, 0x4400
+  stw r19, 0(r20)
+  nop 
+  .word 0x00000000
+  lfs f17, 16484(r17)
+  .word 0x00000017
+  li r4, 0xFFFF
+  lis r20, 0x8039
+  ori r20, r20, 0x21D9
+  lis r19, 0x4E41
+  ori r19, r19, 0x4749
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x21DD
+  lis r19, 0x5441
+  ori r19, r19, 0x4F53
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x21E1
+  lis r19, 0x4849
+  ori r19, r19, 0x47
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x21E5
+  lis r19, 0x4154
+  ori r19, r19, 0x4520
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x21E9
+  lis r19, 0x4348
+  ori r19, r19, 0x414C
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x21ED
+  lis r19, 0x4C45
+  ori r19, r19, 0x4E47
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x21F1
+  lis r19, 0x4500
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x21F5
+  lis r19, 0x5452
+  ori r19, r19, 0x4941
+  stw r19, 0(r20)
+  lis r20, 0x8039
+  ori r20, r20, 0x21F9
+  lis r19, 0x4C00
+  stw r19, 0(r20)
